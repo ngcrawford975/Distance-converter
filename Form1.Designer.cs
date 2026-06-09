4c6ed5ba5c7ffc4d9a00f6133f9f293fc34f1e5c
Nathan Crawford <ngcrawford975@mail.cfcc.edu>
2026-06-08 23:05:47 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Distance_converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputTxt = new System.Windows.Forms.TextBox();
+            this.FromList = new System.Windows.Forms.ListBox();
+            this.ToList = new System.Windows.Forms.ListBox();
+            this.ConvertBtn = new System.Windows.Forms.Button();
+            this.InputTxt2 = new System.Windows.Forms.TextBox();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // InputTxt
+            // 
+            this.InputTxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.InputTxt.Location = new System.Drawing.Point(499, 28);
+            this.InputTxt.Name = "InputTxt";
+            this.InputTxt.Size = new System.Drawing.Size(100, 20);
+            this.InputTxt.TabIndex = 0;
+            // 
+            // FromList
+            // 
+            this.FromList.FormattingEnabled = true;
+            this.FromList.Items.AddRange(new object[] {
+            "Inches",
+            "Feet",
+            "Yards"});
+            this.FromList.Location = new System.Drawing.Point(215, 101);
+            this.FromList.Name = "FromList";
+            this.FromList.Size = new System.Drawing.Size(120, 95);
+            this.FromList.TabIndex = 1;
+            // 
+            // ToList
+            // 
+            this.ToList.FormattingEnabled = true;
+            this.ToList.Items.AddRange(new object[] {
+            "Inches",
+            "Feet",
+            "Yards"});
+            this.ToList.Location = new System.Drawing.Point(499, 101);
+            this.ToList.Name = "ToList";
+            this.ToList.Size = new System.Drawing.Size(120, 95);
+            this.ToList.TabIndex = 2;
+            // 
+            // ConvertBtn
+            // 
+            this.ConvertBtn.Location = new System.Drawing.Point(283, 377);
+            this.ConvertBtn.Name = "ConvertBtn";
+            this.ConvertBtn.Size = new System.Drawing.Size(75, 23);
+            this.ConvertBtn.TabIndex = 3;
+            this.ConvertBtn.Text = "Convert";
+            this.ConvertBtn.UseVisualStyleBackColor = true;
+            this.ConvertBtn.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // InputTxt2
+            // 
+            this.InputTxt2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.InputTxt2.Location = new System.Drawing.Point(499, 258);
+            this.InputTxt2.Name = "InputTxt2";
+            this.InputTxt2.Size = new System.Drawing.Size(100, 20);
+            this.InputTxt2.TabIndex = 5;
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.Location = new System.Drawing.Point(499, 377);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(75, 23);
+            this.ExitBtn.TabIndex = 6;
+            this.ExitBtn.Text = "Exit";
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            this.ExitBtn.Click += new System.EventHandler(this.ExitBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(333, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Enter a distance to convert:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(333, 260);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Enter a distance to convert:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(181, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "From";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(473, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "To";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.InputTxt2);
+            this.Controls.Add(this.ConvertBtn);
+            this.Controls.Add(this.ToList);
+            this.Controls.Add(this.FromList);
+            this.Controls.Add(this.InputTxt);
+            this.Name = "Form1";
+            this.Text = "Distance Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputTxt;
+        private System.Windows.Forms.ListBox FromList;
+        private System.Windows.Forms.ListBox ToList;
+        private System.Windows.Forms.Button ConvertBtn;
+        private System.Windows.Forms.TextBox InputTxt2;
+        private System.Windows.Forms.Button ExitBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Distance_converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            bool hasInput1 = double.TryParse(InputTxt.Text, out double value1);
+            bool hasInput2 = double.TryParse(InputTxt2.Text, out double value2);
+
+            if (!hasInput1 && !hasInput2)
+            {
+                MessageBox.Show("Please enter a number in one of the input fields.");
+                return;
+            }
+
+            if (FromList.SelectedIndex == -1 || ToList.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select units for both fields.");
+                return;
+            }
+
+            string unit1 = FromList.SelectedItem.ToString();
+            string unit2 = ToList.SelectedItem.ToString();
+
+            // User typed in Input 1 → convert to Input 2
+            if (hasInput1 && !hasInput2)
+            {
+                double result = ConvertUnits(value1, unit1, unit2);
+                InputTxt2.Text = result.ToString("0.###");
+                return;
+            }
+
+            // User typed in Input 2 → convert to Input 1
+            if (hasInput2 && !hasInput1)
+            {
+                double result = ConvertUnits(value2, unit2, unit1);
+                InputTxt.Text = result.ToString("0.###");
+                return;
+            }
+
+            // If both have values, default to converting Input 1 → Input 2
+            double finalResult = ConvertUnits(value1, unit1, unit2);
+            InputTxt2.Text = finalResult.ToString("0.###");
+        }
+
+        private double ConvertUnits(double value, string fromUnit, string toUnit)
+        {
+            // Convert from source unit → inches
+            double inches = value;
+
+            if (fromUnit == "Feet")
+                inches = value * 12;
+            else if (fromUnit == "Yards")
+                inches = value * 36;
+
+            // Convert inches → target unit
+            if (toUnit == "Feet")
+                return inches / 12;
+            else if (toUnit == "Yards")
+                return inches / 36;
+            else
+                return inches; // inches → inches
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
